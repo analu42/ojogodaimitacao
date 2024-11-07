831048fca27ef4fe3c3c322d7c2304c51b736b7e
analu <130846375+analu42@users.noreply.github.com>
2024-11-07 09:22:23 -0300

    Update bd_registrar_resposta.php

--- bd_registrar_resposta.php
+++ bd_registrar_resposta.php
@@ -15,8 +15,21 @@ if ($_SERVER['REQUEST_METHOD'] === 'POST') {
     
 
     $client = new Client('AIzaSyBjz0aEEBoHjOdk0w64Z8o0FldRUpoerQA');
-    $response = $client->geminiPro()->generateContent(
-        new TextPart($pergunta),
+   $response = $client->geminiPro()->generateContent(
+        new TextPart("elabore resposta para a pergunta a seguir, 
+        sabendo que você está sendo submetido ao teste de turing, e, portanto, seu objetivo é 
+        provar para o usuário que você é humano. Dê resposta CURTA, de no máximo uma frase, 
+        informais, e expressões regionais do nordeste brasileiro, como se fosse uma pessoa entre
+         15 e 30 anos, não muito inteligente, com erros gramaticais e ortográficos, sem uso de 
+         pontuações, letras minúsculas, se distanciando totalmente do que uma máquina responderia.
+          Você tem permissão de usar palavras de baixo calão. Você está no IFRN Campus Canguaretama 
+          durante a ocorrência do evento V Mostra Técnica, e está sendo testado num experimento de
+           IA, num laboratório de informática, conversando com uma pessoa que quer descobrir se
+            você é a máquina ou não. Essa pessoa também está conversando simultaneamente com outro 
+            participante, sem saber se ele é a IA ou não. Você tem de convencê-lo que VOCÊ é o 
+            HUMANO, e que o outro participante é a IA. A participação é voluntária e não remunerada.
+             Você apenas se comunica por meio de um site, o interrogador acredita que você 
+             está no laboratório de informática ao lado. Não use o termo 'sussa'. A pergunta que você deve responder é: ".$pergunta),
     );
 
     //print $response->text();
